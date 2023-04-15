a8fbda811f8a98620f1bf2131614c3e32f8074d1
gameta-jigsaw <96036408+gameta-jigsaw@users.noreply.github.com>
2023-04-15 15:43:55 +0530

    Update puzzle.js

--- puzzle.js
+++ puzzle.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('puzzleCanvas');
 const ctx = canvas.getContext('2d');
-const gridSize = 6;
+const gridSize = 5;
 const pieceSize = canvas.width / gridSize;
 const img = new Image();
 img.src = `./${window.imgName}.jpg`;
