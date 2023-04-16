0570ae67cc6380fc84dbdc6533aa52d7f6e9142e
gameta-jigsaw <96036408+gameta-jigsaw@users.noreply.github.com>
2023-04-17 03:14:17 +0530

    Update puzzle.js

--- puzzle.js
+++ puzzle.js
@@ -121,7 +121,7 @@ function createShuffledPieces() {
   const pieceCount = gridSize * gridSize;
   const pieces = Array.from({ length: pieceCount }, (_, i) => i);
 
-  const numberOfMoves = 70; // Adjust this value to change the difficulty level
+  const numberOfMoves = 75; // Adjust this value to change the difficulty level
 
   let emptyPieceIndex = pieceCount - 1;
 
